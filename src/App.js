77edbdabe5df1183e90993ee1aee88f8b69d0193
sablin <sablin.irk@gmail.com>
2021-03-03 11:03:17 +0800

    овая версия

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import logo from './logo.svg';
+import {useState} from 'react'
+import logo from './images/logo.png';
 import './App.css';
 
 function App() {
+
+   const [formState, setFormState] = useState(0);
+   const [formRating, setFormRating] = useState(1)
+
+   let data = new FormData();
+   data.append('phone', '89041544527')
+   data.append('id', 1009)
+   const formSubmit = (e)=>{
+     e.preventDefault();
+    console.log(data)
+     if(formRating < 5){
+      fetch('https://flashformat.pro/moneyshop/api/2/index.php',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      },
+      body: data
+      }).then((res)=>console.log(res))
+     }else{
+       setFormState(1)
+     }
+    }
+  const onFormChange= (e)=>{
+    setFormRating(e.target.value)
+    }
+
   return (
     <div className="App">
-     <form>
-       <h1>Добрый день! Будем рады если Вы оцените качество нашей работы по 5-бальной шкале!</h1>
-       <div className="inputBox">
-         <div><input type="radio" name="star" value="1" />
-         <label>&#11088;</label> </div>
-         <div><input type="radio" name="star" value="1" />
-         <label>&#11088;&#11088;</label> </div>
-         <div><input type="radio" name="star" value="1" />
-         <label>&#11088;&#11088;&#11088;</label> </div>
-         <div><input type="radio" name="star" value="1" />
-         <label>&#11088;&#11088;&#11088;&#11088;</label> </div>
-         <div><input type="radio" name="star" value="1" />
-         <label>&#11088;&#11088;&#11088;&#11088;&#11088;</label> </div>
+      {formState === 0 ? 
+           <form className="formData" onSubmit={formSubmit}>
+             <img className="logo" src={logo} />
+           <h1>Добрый день! Будем рады если Вы оцените качество нашей работы по 5-бальной шкале!</h1>
         
-       </div>
-       <input type="submit" value="Отправить отзыв"/>
-     </form>
+           <div className="rating-area">
+	<input type="radio" id="star-5" name="rating" value="5" onChange={onFormChange}/>
+	<label htmlFor="star-5" title="Оценка «5»"></label>	
+	<input type="radio" id="star-4" name="rating" value="4" onChange={onFormChange}/>
+	<label htmlFor="star-4" title="Оценка «4»"></label>    
+	<input type="radio" id="star-3" name="rating" value="3" onChange={onFormChange}/>
+	<label htmlFor="star-3" title="Оценка «3»"></label>  
+	<input type="radio" id="star-2" name="rating" value="2" onChange={onFormChange}/>
+	<label htmlFor="star-2" title="Оценка «2»"></label>    
+	<input type="radio" id="star-1" name="rating" value="1" onChange={onFormChange}/>
+	<label htmlFor="star-1" title="Оценка «1»"></label>
+</div>
+         <input type="submit" value="Отправить"/>
+         </form>
+         :
+         <form>
+         <h1>Большое спасибо за оценку!</h1>
+         <h2>Получите <span>100</span> бонусных баллов на карту клиента*, оставив отзыв по одной из ссылок.</h2>
+         <div className="inputBox">
+           <div><a href='http://2gis.ru'> 2gis</a></div>
+           <div><a href='http://google.ru'> Google</a></div>
+           <div><a href='http://flamp.ru'> Flamp</a> </div>
+         </div>
+         <p>* Для получения бонусных баллов покажите отзыв в магазине</p>
+       
+       </form>
+
+    }
+
+ 
     </div>
   );
 }
